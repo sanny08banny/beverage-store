be8a7400b21d1c6920c161530e5411da71f15891
sanny08banny <jxtsam08@gmail.com>
2025-05-25 00:30:46 +0300

    Fix: Spline rendering fixed

--- app/components/SplineScene.tsx
+++ app/components/SplineScene.tsx
@@ -3,8 +3,8 @@ import Spline from "@splinetool/react-spline";
 
 const SplineScene: React.FC = () => {
   return (
-    <div className="w-full h-[500px]">
-      <Spline scene="https://prod.spline.design/YOUR-SCENE-URL/scene.splinecode" />
+    <div className="w-full h-[800px]">
+      <Spline scene="https://prod.spline.design/YGQilcF08shjZfeS/scene.splinecode" />
     </div>
   );
 };
